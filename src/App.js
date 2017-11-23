b108593d6387999d14dbeb82c43d87ad40918936
nahom <nahumtefera@gmail.com>
2017-11-23 06:23:22 -0800

    navbar completely functional

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import './app.css';
 import Header from './components/header/header';
-import Headline from './components/headline/headline';
-import ServicesPanel from './components/services-panel/services-panel';
-import Portfolio from './components/portfolio/portfolio';
+// import Headline from './components/headline/headline';
+// import ServicesPanel from './components/services-panel/services-panel';
+// import Portfolio from './components/portfolio/portfolio';
 
 class App extends Component {
   render() {
