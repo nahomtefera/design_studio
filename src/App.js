af108ea486c8401043152ceff4bbccc079785ee5
nahom <nahumtefera@gmail.com>
2018-01-10 18:07:02 -0800

    created two functions 'start_request' and 'pause_request' to open and close the window that will pop up when we want to start a new service

--- src/App.js
+++ src/App.js
@@ -9,6 +9,34 @@ import GetStarted from './components/getstarted/getstarted';
 import Footer from './components/footer/footer';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      start_request: false,
+      request_servie: "",
+    }
+
+    this.startRequest = this.startRequest.bind(this);
+    this.pauseRequest = this.pauseRequest.bind(this);
+  }
+
+  // This function will open the window to start the process
+  // of chosing/requesting a service
+  startRequest(){
+    this.setState({
+      start_request: true,
+    })
+  }
+  // This function will close the window to pause the process
+  // of chosing/requesting a service
+  pauseRequest(){
+    this.setState({
+      start_request: false
+    })
+  }
+
   render() {
     return (
       <div className="App">
